718742758c804f5d2fdab064f948e96355c733c0
Victor Hugo <hugooandradec@gmail.com>
2026-04-05 02:28:14 -0300

    auto commit

--- app-operacao-react/vite.config.js
+++ app-operacao-react/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/app-operacao-react/dist/",
+  base: "/webapp/app-operacao-react/dist/",
 });
